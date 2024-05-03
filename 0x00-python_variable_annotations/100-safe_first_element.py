1df3d017e6033a9d13ae3a32776c9c34b0a2e541
eminentetim <etimeminent@gmail.com>
2024-05-03 05:46:43 +0100

    update

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,16 +1,14 @@
+#!/usr/bin/env python3
+'''Task 9's module.
+'''
 from typing import Any, Optional, List
 
+
 def safe_first_element(lst: List[Any]) -> Optional[Any]:
-    """
-    Returns the first element of a list if it exists, otherwise returns None.
-
-    Args:
-        lst (List[Any]): A list of elements of any type.
-
-    Returns:
-        Optional[Any]: The first element if the list is not empty, otherwise None.
-    """
+    '''Computes the length of a list of sequences.
+    '''
     if lst:
-        return lst[0]
+         return lst[0]
     else:
-        return None
+         return None
+
